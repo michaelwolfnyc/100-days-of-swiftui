d7d311e035dcbf20144835ab6a9cfc7fa8681d16
Michael Wolf <michaelwolfnyc@gmail.com>
2022-01-11 17:40:44 -0500

    ChallengeTimesTables as good as will get for now; functionality there

--- ChallengeTimesTables/ChallengeTimesTables/ContentView.swift
+++ ChallengeTimesTables/ChallengeTimesTables/ContentView.swift
@@ -10,15 +10,33 @@ import SwiftUI
 enum GamePhase {
     case Settings
     case Playing(Int)
+    case GameOver
+
+    mutating func incrementQuestion(nQuestions: Int) {
+        switch self {
+        case .Settings:
+            self = .Playing(0)
+        case .Playing(let n):
+            assert(n >= 0 && n < nQuestions)
+            if n == nQuestions - 1 {
+                self = .GameOver
+            } else {
+                self = .Playing(n+1)
+            }
+        case .GameOver:
+            assert(false)
+        }
+    }
 }
 
 struct Question {
-    let q: String
-    let a: Int
+    let m1: Int
+    let m2: Int
     let qNumber: Int
 }
 
 struct SettingsView: View {
+    @Binding var correct: Int
     @Binding var gamePhase: GamePhase
     @Binding var questionList: [Question]
     @State var questions = 1.0
@@ -29,13 +47,12 @@ struct SettingsView: View {
         withAnimation {
             gamePhase = .Playing(0)
         }
+        correct = 0
         questionList = []
         for qNumber in 0..<Int(questions) {
             let m1 = Int.random(in: Int(smallest)...Int(largest))
             let m2 = Int.random(in: Int(smallest)...Int(largest))
-            let q = "\(m1) \u{00D7} \(m2)"
-            let a = m1 * m2
-            questionList.append(Question(q:q, a: a, qNumber: qNumber))
+            questionList.append(Question(m1: m1, m2: m2, qNumber: qNumber))
         }
     }
 
@@ -74,17 +91,88 @@ struct SettingsView: View {
 
 
 struct ContentView: View {
-    @State var gamePhase: GamePhase = .Settings
-    @State var questionList: [Question] = []
+    @State private var gamePhase: GamePhase = .Settings
+    @State private var questionList: [Question] = []
+    @State private var correct = 0
+    @State private var answer = ""
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
+    func checkAnswer() {
+        var expectedAnswer = -1
+        switch gamePhase {
+        case .Settings:
+            assert(false)
+        case .Playing(let qNumber):
+            expectedAnswer = questionList[qNumber].m1 * questionList[qNumber].m2
+            alertMessage = "\(questionList[qNumber].m1) \u{00D7} \(questionList[qNumber].m2) = \(expectedAnswer)"
+        case .GameOver:
+            assert(false)
+        }
+        let a = Int(answer)
+        print("answer         = <<\(answer)>>")
+        print("a              = <<\(a)>>")
+        print("expectedAnswer = <<\(expectedAnswer)>>")
+        if (a ?? -1) == expectedAnswer {
+            correct += 1
+            gamePhase.incrementQuestion(nQuestions: questionList.count)
+            answer = ""
+        } else {
+            showingAlert = true
+            
+        }
+    }
     
     var body: some View {
         switch gamePhase {
         case .Settings:
-            SettingsView(gamePhase: $gamePhase, questionList: $questionList)
+            SettingsView(correct: $correct, gamePhase: $gamePhase, questionList: $questionList)
         case .Playing(let qNumber):
-            List(questionList, id: \.qNumber) { question in
-                Text("q: \(question.q) a: \(question.a)")
+            List(questionList, id: \.qNumber) { q in
+                if q.qNumber == qNumber {
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Text("Score:")
+                            Text("\(correct) / \(qNumber)")
+                        }
+                        Spacer()
+                        Spacer()
+                        Text("Question \(qNumber+1)")
+                        Spacer()
+                        Text("\(q.m1) \u{00D7} \(q.m2)").font(.largeTitle)
+                        Spacer()
+                        TextField("Your answer", text: $answer).font(.largeTitle)
+                            .padding()
+                            .foregroundColor(.black)
+                            .background(Color.yellow)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .onSubmit(checkAnswer)
+                        Spacer()
+                    }.alert(alertMessage, isPresented: $showingAlert) {
+                        Button("OK", role: .cancel) {
+                            gamePhase.incrementQuestion(nQuestions: questionList.count)
+                            answer = ""
+                            
+                        }
+                    }
+                }
             }
+        case .GameOver:
+            Text("Game Over")
+            HStack {
+                Text("Score:")
+                Text("\(correct) / \(questionList.count)")
+            }
+
+            HStack(alignment: .center) {
+                Button("Play Again") {gamePhase = .Settings}
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+
         }
     }
 }
---
+++ ChallengeTimesTables/ChallengeTimesTables/README.md
@@ -0,0 +1,9 @@
+#  README
+
+Ugly, but functionality is there.
+
+1.  Code rot already.  E.g. answer = "" and incrementQuestion() always paired, and incrementQuestion always uses questionList.count.
+Possibly a class encapsulates that kind of stuff, and other state too?
+2.  Game over screen, no possibility to quit, though apps don't allow that, so maybe not play again? button
+3.  I'm sure multiple screens not supposed to be done with a state variable and an if statement!
+

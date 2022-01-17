43162162684c55086c415b0a4367fac095896ef7
Michael Wolf <michaelwolfnyc@gmail.com>
2022-01-17 10:20:11 -0500

    Moonshot version 1

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Moonshot
+//
+//  Created by Michael Wolf on 1/16/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

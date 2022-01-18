c41e0b25128b22f99a96035d70213b0b4f232e3f
Michael Wolf <michaelwolfnyc@gmail.com>
2022-01-17 19:05:39 -0500

    Moonshot version 2: two of the three challenges

---
+++ Moonshot/Moonshot/BetterSpacer.swift
@@ -0,0 +1,18 @@
+//
+//  BetterSpacer.swift
+//  Moonshot
+//
+//  Created by Michael Wolf on 1/17/22.
+//
+
+import SwiftUI
+
+struct BetterSpacer: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundColor(.lightBackground)
+            .padding(.vertical)
+    }
+}
+
--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -7,14 +7,50 @@
 
 import SwiftUI
 
+struct CrewMember {
+    let role: String
+    let astronaut: Astronaut
+}
+
+struct HorizontalScrollViewForMission: View {
+    let crew: [CrewMember]
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(Capsule())
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundColor(.white)
+                                    .font(.headline)
+                                Text(crewMember.role)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+
+    }
+}
+
 struct MissionView: View {
     let mission: Mission
     let crew: [CrewMember]
-    
-    struct CrewMember {
-        let role: String
-        let astronaut: Astronaut
-    }
     
     init(mission: Mission, astronauts: [String: Astronaut]) {
         self.mission = mission
@@ -36,60 +72,33 @@ struct MissionView: View {
                         .resizable()
                         .scaledToFit()
                         .frame(maxWidth: geometry.size.width * 0.6)
-                        .padding(.top)
+                        .padding()
+                    
+                    Text("Launch Date: " +
+                         (mission.launchDate == nil ? "(Unknown)":
+                          mission.formattedLaunchDate))
+                        .foregroundColor(.secondary)
 
                     VStack(alignment: .leading) {
-                        Rectangle()
-                            .frame(height: 2)
-                            .foregroundColor(.lightBackground)
-                            .padding(.vertical)
-                        
+
+                        BetterSpacer()
+                       
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
 
                         Text(mission.description)
                         
-                        Rectangle()
-                            .frame(height: 2)
-                            .foregroundColor(.lightBackground)
-                            .padding(.vertical)
-                        
+                        BetterSpacer()
+
                         Text("Crew")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                     }
                     .padding(.horizontal)
-                    
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(crew, id: \.role) { crewMember in
-                                NavigationLink {
-                                    AstronautView(astronaut: crewMember.astronaut)
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width: 104, height: 72)
-                                            .clipShape(Capsule())
-                                            .overlay(
-                                                Capsule()
-                                                    .strokeBorder(.white, lineWidth: 1)
-                                            )
 
-                                        VStack(alignment: .leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundColor(.white)
-                                                .font(.headline)
-                                            Text(crewMember.role)
-                                                .foregroundColor(.secondary)
-                                        }
-                                    }
-                                    .padding(.horizontal)
-                                }
-                            }
-                        }
-                    }
+                    HorizontalScrollViewForMission(crew: crew)
+                    
                 }
                 .padding(.bottom)
             }
